262cfbc75c5065d83be942e2bd86af9f6e5fbcdb
Matheus LC Santos <matheuslcsantosbh@gmail.com>
2023-11-23 14:55:35 -0300

    Add files via upload

---
+++ atividadeFinal/atividadeFinal/ContaBancaria.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadeFinal
+{
+    internal class ContaBancaria
+    {
+        private int NumeroDaConta;
+        private string Titular;
+        private string DepositoInicial;
+        private float ValorEmConta;
+
+        public ContaBancaria(int numeroDaConta, string titular , float valorEmConta)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titular;
+            ValorEmConta = valorEmConta;
+        }
+
+        public ContaBancaria(int numeroDaConta, string titular)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titular;
+        }
+
+        public ContaBancaria() { }
+
+
+
+        public int GetNumeroDaConta() { return NumeroDaConta; }
+
+
+        public void setNumeroDaConta(int valor)
+        {
+            NumeroDaConta = valor;
+        }
+        public string getTitular() { return Titular; }
+
+        public void setTitular(string valor)
+        {
+            Titular = valor;
+        }
+        public float GetValorEmConta() { return ValorEmConta; }
+
+        public void setValorEmConta(float valor){
+            ValorEmConta= valor;
+        }
+        public string getDepositoInicial() {  return DepositoInicial; }
+
+        public void setDepositoInicial(string valor)
+        {
+            DepositoInicial = valor;
+        }
+
+        public void Deposito(float quantia)
+        {
+            ValorEmConta += quantia;
+        }
+
+        public void Saque(float quantia)
+        {
+            ValorEmConta -= quantia + 5;
+        }
+
+
+        public void CadastroCB()
+        {
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            setNumeroDaConta(numero);
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            setTitular(titular);
+
+
+        }
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroDaConta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + ValorEmConta.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+        
+
+
+
+    }
+}
---
+++ atividadeFinal/atividadeFinal/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadeFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool opc = false;
+            ContaBancaria conta = new ContaBancaria();
+
+            do
+            {
+                int selec = menu();
+                Console.Clear();
+
+                switch (selec)
+                {
+                    case 1:
+                        conta.CadastroCB();
+                        break;
+                    case 2:
+                        Console.WriteLine(conta);
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite a baixo a quantia a ser depositada: ");
+                        float quantiaDeposito = float.Parse(Console.ReadLine());
+                        conta.Deposito(quantiaDeposito);
+                        break;
+                    case 4:
+                        Console.WriteLine("Digite a quantia a ser sacada: ");
+                        float quantiaSaque = float.Parse(Console.ReadLine());
+                        conta.Saque(quantiaSaque);
+                        break;
+                    case 0:
+                        opc = true; 
+                    break;
+                }
+            }
+            while (!opc);
+        }
+
+        static int menu()
+        {
+            Console.WriteLine("Menu:\r\n ========================================\r\n1 - Cadastrar conta Bancaria\r\n2 - Mostrar dados da Conta\r\n3 - Realizar deposito\r\n4 - Realizar saque\r\n0 - Sair\r\nSelecione uma opção: ");
+            int x = int.Parse(Console.ReadLine());
+            return x;
+        }
+
+    }
+}
